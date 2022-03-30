4566bf0e4c6ca2ef0be36062d55fda7099401efc
samnomadgate <81360115+samnomadgate@users.noreply.github.com>
2022-03-30 19:37:23 +0300

    layout of homepage

--- index.js
+++ index.js
@@ -8,11 +8,9 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs'); 
 app.set('views', path.join(__dirname, '/views'));
 
-
 app.get('/', (req, res) => {
   res.render('index.ejs');
 });
-
 
 app.listen(3000, () => {
   console.log('listening on port 3000');
